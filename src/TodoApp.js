2387c7c2813736bbca648afdb7a6ed29a1fe3ae9
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-22 16:54:39 +0530

    Added delete and toggle completion

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,6 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
+import uuid from "uuid/v4";
 
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
@@ -16,8 +17,21 @@ function TodoApp() {
     { id: 3, task: "Grow Beard", completed: false }
   ];
   const [todos, setTodos] = useState(initialTodos);
+
   const addTodo = newTodoText => {
-    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
+  };
+
+  const removeTodo = todoId => {
+    const updatedTodos = todos.filter(todo => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+
+  const toggleTodo = todoId => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
   };
 
   return (
@@ -35,8 +49,16 @@ function TodoApp() {
           <Typography color="inherit">TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} />
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          />
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,19 +2,24 @@ import React from "react";
 
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
 
-function TodoList(props) {
+import Todo from "./Todo";
+
+function TodoList({ todos, removeTodo, toggleTodo }) {
   return (
     <Paper>
       <List>
-        {props.todos.map(todo => (
+        {todos.map(todo => (
           <>
-            <ListItem>
-              <ListItemText>{todo.task}</ListItemText>
-            </ListItem>
+            <Todo
+              task={todo.task}
+              key={todo.id}
+              completed={todo.completed}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              id={todo.id}
+            />
             <Divider />
           </>
         ))}

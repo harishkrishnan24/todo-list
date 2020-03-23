0686b850aebd9e10bceed0a63a60812031aa8037
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-23 20:21:19 +0530

    Added local storage custom hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
@@ -12,14 +12,10 @@ import TodoForm from "./TodoForm";
 import useTodoState from "./hooks/useTodoState";
 
 function TodoApp() {
-  const initialTodos = JSON.parse(window.localStorage.getItem("todos")) || [];
+  const initialTodos = [{ id: 1, task: "Watch NBA", completed: false }];
   const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(
     initialTodos
   );
-
-  useEffect(() => {
-    window.localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
 
   return (
     <Paper
--- src/hooks/useTodoState.js
+++ src/hooks/useTodoState.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
 import uuid from "uuid/v4";
 
+import useLocalStorageState from "./useLocalStorageState";
+
 export default initialTodos => {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useLocalStorageState("todos", initialTodos);
 
   return {
     todos,
